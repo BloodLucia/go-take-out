ce2cb7dc207dbf170e5c2823853add2a39224498
kalougata <kalougata@111.com>
2023-10-24 11:37:04 +0000

    使用fiber的BodyParser

--- internal/controller/admin/auth_controller.go
+++ internal/controller/admin/auth_controller.go
@@ -1,12 +1,28 @@
 package adminctrl
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type AuthController struct {
 }
 
+type AdminLoginRequest struct {
+	LoginName string `json:"login_name"`
+	Passwd    string `json:"passwd"`
+}
+
 func (ac *AuthController) Login() fiber.Handler {
 	return func(c *fiber.Ctx) error {
+		var req = new(AdminLoginRequest)
+		if err := c.BodyParser(req); err != nil {
+			c.SendStatus(http.StatusUnprocessableEntity)
+			return c.JSON(fiber.Map{
+				"errs": err.Error(),
+			})
+		}
 		return c.SendString("Login")
 	}
 }
